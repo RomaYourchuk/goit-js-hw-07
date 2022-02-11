4a38f05b2589a82eee72aaf9a1964bd39c4b74f2
RomaYourchuk <yourchuk922@gmail.com>
2022-02-11 15:38:18 +0200

    рендер розмітки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery")
+const cardsMarkup = createGalleryMarkup(galleryItems)
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description })=> {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>`
+        }).join('')
+    
+}
+
+// import basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+//     width="800" height="600">
+// `)
+
+// instance.show()
+
+
+
+
+
